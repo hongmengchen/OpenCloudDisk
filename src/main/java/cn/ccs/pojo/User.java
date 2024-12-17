c0cf042ba9465fe9eb3ae20c2af47cd0bda23d60
hongmengchen <2560174991@qq.com>
2024-12-17 16:41:25 +0800

    refactor(pojo): 重构 User 类并添加 Lombok 注解- 引入 Lombok 库的 @Data 注解，自动生成 getter 和 setter 方法 - 删除手动编写的 getter 和 setter 方法，简化代码 - 添加类的注释和字段的注释，提高代码可读性

--- src/main/java/cn/ccs/pojo/User.java
+++ src/main/java/cn/ccs/pojo/User.java
@@ -1,59 +1,25 @@
 package cn.ccs.pojo;
 
+import lombok.Data;
+
+/**
+ * 用户实体类
+ */
+
+@Data
 public class User {
+    // 命名空间
     public static final String NAMESPACE = "username";
+    // 删除
     public static final String RECYCLE = "recycle";
+    // 用户id，自增
     private Integer id;
+    // 用户名
     private String username;
+    // 密码
     private String password;
+    // 已上传文件大小
     private String countSize;
+    // 可用总空间大小
     private String totalSize;
-
-    public String getTotalSize() {
-        return totalSize;
-    }
-
-    public void setTotalSize(String totalSize) {
-        this.totalSize = totalSize;
-    }
-
-    public String getCountSize() {
-        return countSize;
-    }
-
-    public void setCountSize(String countSize) {
-        this.countSize = countSize;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getRecycle() {
-        return RECYCLE;
-    }
-
-    public String getNamespace() {
-        return NAMESPACE;
-    }
 }
