a6c2d16d4d7674072330c9b6f4e877c0763f5b28
hongmengchen <2560174991@qq.com>
2024-12-17 23:28:39 +0800

    refactor UserService and UserServiceImpl
    - 为 UserService 接口添加了文档注释，明确了方法的用途和参数
    - 为 UserServiceImpl 类添加了详细的文档注释，包括构造函数和方法的说明- 优化了代码格式，调整了缩进和空格
    - 重构了 getCountSize 方法，提高了代码的可读性和健壮性

--- src/main/java/cn/ccs/service/UserService.java
+++ src/main/java/cn/ccs/service/UserService.java
@@ -2,9 +2,24 @@ package cn.ccs.service;
 
 import cn.ccs.pojo.User;
 
+/**
+ * 用户服务接口
+ */
+
 public interface UserService {
-    // 查询用户
+    /**
+     * 查询用户信息
+     *
+     * @param user 用户对象，包含查询条件
+     * @return 返回查询到的用户对象
+     */
     User findUser(User user);
-    // 获取当前用户所用空间
+
+    /**
+     * 获取当前用户所用空间
+     *
+     * @param username 用户名
+     * @return 返回用户的使用空间大小
+     */
     String getCountSize(String username);
 }
--- src/main/java/cn/ccs/service/impl/UserServiceImpl.java
+++ src/main/java/cn/ccs/service/impl/UserServiceImpl.java
@@ -7,48 +7,89 @@ import cn.ccs.utils.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * 用户服务实现类
+ */
+
 @Service("userService")
 public class UserServiceImpl implements UserService {
-    // 注入Dao
+
+    // 用户数据访问对象，用于与数据库进行交互
     private final UserDao userDao;
 
+    /**
+     * 构造函数，用于注入UserDao实例
+     *
+     * @param userDao 用户数据访问对象，不能为空，用于执行数据库操作
+     */
     @Autowired
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
     }
 
+    /**
+     * 根据用户信息查找用户
+     *
+     * @param user 用户对象，包含查找用户的必要信息
+     * @return 如果找到用户则返回用户对象，否则返回null
+     */
     @Override
-    public User findUser(User user){
+    public User findUser(User user) {
         try {
+            // 对用户密码进行MD5加密处理
             user.setPassword(UserUtils.MD5(user.getPassword()));
+            // 调用Dao方法查找用户
             User existUser = userDao.findUser(user);
-            return  existUser;
-        }catch (Exception e){
+            return existUser;
+        } catch (Exception e) {
+            // 异常处理，打印堆栈跟踪信息
             e.printStackTrace();
+            // 返回null表示查找失败
             return null;
         }
     }
 
+    /**
+     * 获取用户的当前所用空间大小
+     *
+     * @param username 用户名
+     * @return 所用空间大小的字符串表示，如果发生异常则返回null
+     */
     @Override
     public String getCountSize(String username) {
         String countSize = null;
-		try {
-			countSize = userDao.getCountSize(username);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return countSize;
-		}
-		return countSize;
+        try {
+            // 调用Dao方法获取所用空间大小
+            countSize = userDao.getCountSize(username);
+        } catch (Exception e) {
+            // 调用Dao方法获取所用空间大小
+            e.printStackTrace();
+            // 返回当前countSize，可能为null，表示获取失败
+            return countSize;
+        }
+        // 返回所用空间大小
+        return countSize;
     }
 
-    public boolean addUser(User user) throws Exception{
-        User users=userDao.checkUser(user);
-        if(users==null) {
+    /**
+     * 添加新用户
+     *
+     * @param user 待添加的用户对象
+     * @return 添加成功返回true，用户已存在返回false
+     * @throws Exception 如果用户已存在或其它原因导致添加失败，抛出异常
+     */
+    public boolean addUser(User user) throws Exception {
+        // 检查用户是否已存在
+        User users = userDao.checkUser(user);
+        if (users == null) {
+            // 用户不存在，对密码进行MD5加密处理后添加到数据库
             user.setPassword(UserUtils.MD5(user.getPassword()));
             userDao.addUser(user);
-        } 	else {
+        } else {
+            // 用户已存在，返回false
             return false;
         }
+        // 添加成功，返回true
         return true;
     }
 }
