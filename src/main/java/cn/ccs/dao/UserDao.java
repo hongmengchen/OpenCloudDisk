5f20b6c2d4790a529303c8a1bac4d2851b75b7de
hongmengchen <2560174991@qq.com>
2024-12-17 21:02:48 +0800

    feat(dao): 添加用户相关查询接口
    - 新增 findUser 方法，用于根据账号密码查询用户- 新增 getCountSize 方法，用于根据用户名查询用户空间大小
    - 添加必要的导入语句，优化代码结构

--- src/main/java/cn/ccs/dao/UserDao.java
+++ src/main/java/cn/ccs/dao/UserDao.java
@@ -1,7 +1,13 @@
 package cn.ccs.dao;
 
+import cn.ccs.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 @Mapper
 public interface UserDao {
+    // 根据账号密码查询用户
+    User findUser(User user);
+    // 根据用户名查询用户空间大小
+    String getCountSize(String username);
 }
