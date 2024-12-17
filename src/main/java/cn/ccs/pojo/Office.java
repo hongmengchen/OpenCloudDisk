5b6c63dcb9c49c8d7221762bac3938c404b0a527
hongmengchen <2560174991@qq.com>
2024-12-17 10:01:27 +0800

    refactor(pojo): 使用 Lombok 优化 Office 类
    - 引入 Lombok 库的 @Data 注解
    - 移除原有的 getter 和 setter 方法
    -简化类的结构，提高代码可读性和易维护性

--- src/main/java/cn/ccs/pojo/Office.java
+++ src/main/java/cn/ccs/pojo/Office.java
@@ -1,22 +1,9 @@
 package cn.ccs.pojo;
 
+import lombok.Data;
+
+@Data
 public class Office {
     private String officeId;
     private String officeMd5;
-
-    public String getOfficeId() {
-        return officeId;
-    }
-
-    public void setOfficeId(String officeId) {
-        this.officeId = officeId;
-    }
-
-    public String getOfficeMd5() {
-        return officeMd5;
-    }
-
-    public void setOfficeMd5(String officeMd5) {
-        this.officeMd5 = officeMd5;
-    }
 }
