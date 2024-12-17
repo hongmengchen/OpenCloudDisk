48e30813328493c502ef6b010ac1fef4a7f52f3d
hongmengchen <2560174991@qq.com>
2024-12-17 23:23:03 +0800

    refactor(src/main/java/cn/ccs/utils/UserUtils.java): 优化 UserUtils 类
    - 为 MD5 方法添加了文档注释，明确了参数和返回值的含义- 为 getUsername 方法添加了文档注释，说明了方法的功能和参数
    - 优化了代码格式，包括缩进和换行

--- src/main/java/cn/ccs/utils/UserUtils.java
+++ src/main/java/cn/ccs/utils/UserUtils.java
@@ -10,14 +10,27 @@ import javax.servlet.http.HttpServletRequest;
  */
 
 public class UserUtils {
-    public static String MD5(String password){
-        if(password!=null){
+    /**
+     * 将密码进行MD5加密
+     *
+     * @param password 明文密码
+     * @return 加密后的密码若输入密码为null，则返回null
+     */
+    public static String MD5(String password) {
+        if (password != null) {
             return DigestUtils.md5DigestAsHex(password.getBytes()).toUpperCase();
-        }else{
+        } else {
             return null;
         }
     }
-    public static String getUsername(HttpServletRequest request){
-		return (String) request.getSession().getAttribute(User.NAMESPACE);
-	}
+
+    /**
+     * 从请求中获取当前用户的用户名
+     *
+     * @param request HTTP请求对象
+     * @return 当前用户的用户名如果没有登录用户，则返回null
+     */
+    public static String getUsername(HttpServletRequest request) {
+        return (String) request.getSession().getAttribute(User.NAMESPACE);
+    }
 }
