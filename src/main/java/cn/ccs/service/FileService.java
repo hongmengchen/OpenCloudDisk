7034c9f604b195619a63e2eb2f8a12cc96171915
hongmengchen <2560174991@qq.com>
2024-12-18 10:49:17 +0800

    feat(FileService): 添加文件下载和删除相关方法
    - 新增 downPackage 方法实现文件打包下载功能- 新增 deleteDownPackage 方法实现删除压缩文件包功能

--- src/main/java/cn/ccs/service/FileService.java
+++ src/main/java/cn/ccs/service/FileService.java
@@ -3,6 +3,7 @@ package cn.ccs.service;
 import cn.ccs.pojo.FileCustom;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.List;
 
 /**
@@ -21,6 +22,13 @@ public interface FileService {
     public String getFileName(HttpServletRequest request, String fileName, String username);
 
     public List<FileCustom> listFile(String realPath);
+
+    // 下载文件打包
+    public File downPackage(HttpServletRequest request, String currentPath, String[] fileNames, String username) throws Exception;
+
+    // 删除压缩文件包
+    public void deleteDownPackage(File downloadFile);
+
 
 }
 
