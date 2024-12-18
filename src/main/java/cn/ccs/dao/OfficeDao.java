11c8ef00372e8c42d427f7527154141637aa89ca
Coki_Lee <14337294+coki-lee@user.noreply.gitee.com>
2024-12-18 19:05:29 +0800

    refactor(FileService): 重构文件服务实现类
    - 移除了 FileServiceImpl 类的构造函数注入方式，改为使用 @Autowired 注解进行字段注入
    - 添加了对 OfficeDao 的自动注入
    - 修正了新建命名空间时的默认目录名称（将 "vido" 改为 "video"）
    - 优化了代码结构，提高了代码的可读性和可维护性

--- src/main/java/cn/ccs/dao/OfficeDao.java
+++ src/main/java/cn/ccs/dao/OfficeDao.java
@@ -1,15 +1,12 @@
 package cn.ccs.dao;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 
-@Repository
+@Mapper
 public interface OfficeDao {
-
-    static void addOffice(@Param("officeId") String officeId, @Param("officeMd5") String officeMd5) throws Exception {
-
-    }
-
+    void addOffice(@Param("officeId") String officeId, @Param("officeMd5") String officeMd5) throws Exception;
     String getOfficeId(String officeMd5) throws Exception;
 }
