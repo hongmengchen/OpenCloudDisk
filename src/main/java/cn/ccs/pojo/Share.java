3a5e15538435e7ec26359ce26591d3518b23bf8e
Coki_Lee <14337294+coki-lee@user.noreply.gitee.com>
2024-12-18 19:06:05 +0800

    refactor(share): 重构 Share 类
    - 移除了不必要的注释
    - 添加了 PUBLIC、PRIVATE、CANCEL 和 DELETE 四个静态常量
    - 修改了 shareId 的类型从 Integer 到 String- 重命名了 shareUser 字段，原意为分享人- 调整了 status 字段的类型从 Integer 到 int
    - 删除了冗余的 command 字段，原意为提取码

--- src/main/java/cn/ccs/pojo/Share.java
+++ src/main/java/cn/ccs/pojo/Share.java
@@ -10,21 +10,14 @@ import lombok.Data;
 @Data
 @TableName("share")
 public class Share {
-    // 分享id，自增
-    private Integer shareId;
-
-    // 分享文件的对外地址
+    public static final int PUBLIC = 1;
+    public static final int PRIVATE = 2;
+    public static final int CANCEL = 0;
+    public static final int DELETE = -1;
     private String shareUrl;
-
-    // 被分享文件的路径
+    private String shareId;
+    private String shareUser;
     private String path;
-
-    // 分享人
-    private Integer shareUser;
-
-    // 状态（1-公开 2-加密）
-    private Integer status;
-
-    // 提取码
     private String command;
+    private int status;
 }
