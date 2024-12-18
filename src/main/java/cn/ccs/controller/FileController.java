f4ea4d7c8c9360828ee4b77bd1eb38c034b8caec
Coki_Lee <14337294+coki-lee@user.noreply.gitee.com>
2024-12-18 11:18:49 +0800

    feat(file): 添加文件夹创建功能并优化文件获取接口
    - 新增 addDirectory 方法，用于创建新文件夹
    - 优化 getFiles 方法，增加结果封装
    -引入 FileServiceImpl 服务类

--- src/main/java/cn/ccs/controller/FileController.java
+++ src/main/java/cn/ccs/controller/FileController.java
@@ -2,6 +2,7 @@ package cn.ccs.controller;
 
 import cn.ccs.pojo.FileCustom;
 import cn.ccs.pojo.Result;
+import cn.ccs.service.impl.FileServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,19 +10,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+
 @Controller
-@RequestMapping("/file")
+@RequestMapping("/file")   //  index.js  ajax    file/getFile.action
 public class FileController {
     @Autowired
     private HttpServletRequest request;
     @Autowired
+    private FileServiceImpl fileService;
+
     @RequestMapping("/getFiles")
     public @ResponseBody Result<List<FileCustom>> getFiles(String path){
+        //根据项目路径及用户名、文件名获取上传文件的真实路径
         String realPath = fileService.getFileName(request, path);
         //获取路径下所有的文件信息
         List<FileCustom> listFile = fileService.listFile(realPath);
         //将文件信息封装成Json对象
+        Result<List<FileCustom>> result = new Result<List<FileCustom>>(325,
+                true, "获取成功");
         result.setData(listFile);
         return result;
     }
+
+
+    @RequestMapping("/addDirectory")
+    //添加文件夹
+    public @ResponseBody Result<String> addDirectory(String currentPath, String directoryName) {
+        try {
+            fileService.addDirectory(request, currentPath, directoryName);
+            return new Result<>(336, true, "添加成功");
+        } catch (Exception e) {
+            return new Result<>(331, false, "添加失败");
+        }
+    }
+
 }
