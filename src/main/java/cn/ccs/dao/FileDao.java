21bfdd65461637616e50522128d280781b9dd516
hongmengchen <2560174991@qq.com>
2024-12-18 19:44:43 +0800

    feat(FileDao): 增加文件删除和查询接口
    - 新增查询回收站中数据的方法- 新增查询单个文件的方法
    - 新增删除单个文件的方法
    - 新增根据用户名删除

--- src/main/java/cn/ccs/dao/FileDao.java
+++ src/main/java/cn/ccs/dao/FileDao.java
@@ -1,9 +1,23 @@
 package cn.ccs.dao;
 
+import cn.ccs.pojo.RecycleFile;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface FileDao {
     void insertFiles(@Param("filePath") String filePath, @Param("userName") String userName) throws Exception ;
+
+    // 查询回收站中数据
+    List<RecycleFile> selectFiles(@Param("userName") String userName) throws Exception;
+
+    // 查询文件
+    RecycleFile selectFile(@Param("fileId") int fileId) throws Exception;
+	// 删除文件
+    void deleteFile(@Param("fileId") int fileId, @Param("userName") String userName) throws Exception;
+
+    // 根据当前用户名删除所有的删除记录
+    void deleteFiles(@Param("userName") String userName) throws Exception;
 }
