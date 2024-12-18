b87332a1dd25ed5dea728339bafed7860b296490
hongmengchen <2560174991@qq.com>
2024-12-18 17:19:22 +0800

    refactor(FileController): 优化文件控制器并添加回收站功能
    - 移动目录控制器方法进行了优化
    - 添加了回收站相关代码（已注释）
    - 新增了 RecycleFile 类的导入

--- src/main/java/cn/ccs/controller/FileController.java
+++ src/main/java/cn/ccs/controller/FileController.java
@@ -1,6 +1,7 @@
 package cn.ccs.controller;
 
 import cn.ccs.pojo.FileCustom;
+import cn.ccs.pojo.RecycleFile;
 import cn.ccs.pojo.Result;
 import cn.ccs.pojo.SummaryFile;
 import cn.ccs.service.FileService;
@@ -218,6 +219,7 @@ public class FileController {
             return new Result<>(351, false, "重命名失败");
         }
     }
+
     /**
      * 移动目录控制器
      *
@@ -238,4 +240,18 @@ public class FileController {
             return new Result<>(361, true, "移动失败");
         }
     }
+
+    /*@RequestMapping("/recycleFile")
+	public String recycleFile() {
+	    try {
+	        List<RecycleFile> findDelFile = fileService.recycleFiles(request);
+	        if(null != findDelFile && findDelFile.size() != 0) {
+	            request.setAttribute("findDelFile", findDelFile);
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return "recycle";
+	}*/
+
 }
