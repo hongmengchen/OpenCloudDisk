2f9281d671a111ed0e3b7a5860a2ef10cf0c349f
hongmengchen <2560174991@qq.com>
2024-12-18 10:17:19 +0800

    refactor(FileController): 重构文件控制器
    - 使用构造函数注入替代字段注入
    - 优化代码结构，提高可读性
    - 添加方法注释，解释功能和参数

--- src/main/java/cn/ccs/controller/FileController.java
+++ src/main/java/cn/ccs/controller/FileController.java
@@ -2,7 +2,7 @@ package cn.ccs.controller;
 
 import cn.ccs.pojo.FileCustom;
 import cn.ccs.pojo.Result;
-import cn.ccs.service.FileService;
+import cn.ccs.service.impl.FileServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +14,31 @@ import java.util.List;
 @Controller
 @RequestMapping("/file")
 public class FileController {
-    @Autowired
-    private HttpServletRequest request;
-    @Autowired
-    private FileService fileService;
-    @RequestMapping("/getFiles")
-    public @ResponseBody Result<List<FileCustom>> getFiles(String path) {
-        String realPath = fileService.getFileName(request, path);
-        //获取路径下所有的文件信息
-        List<FileCustom> listFile = fileService.listFile(realPath);
-        //将文件信息封装成Json对象
-        Result<List<FileCustom>> result = new Result<List<FileCustom>>(325,
-                true, "获取成功");
-        result.setData(listFile);
-        return result;
-    }
+	private final HttpServletRequest request;
+	private final FileServiceImpl fileService;
 
+	@Autowired
+    public FileController(FileServiceImpl fileService, HttpServletRequest request) {
+        this.fileService = fileService;
+		this.request = request;
+	}
+
+	/**
+	 * 获取文件列表
+	 *
+	 * @param path 路径
+	 * @return Json对象
+	 */
+    @RequestMapping("/getFiles")
+	public @ResponseBody Result<List<FileCustom>> getFiles(String path) {
+		//根据项目路径及用户名、文件名获取上传文件的真实路径
+		// WEB-INF/file/ username/ fileName
+		String realPath = fileService.getFileName(request, path);
+		//获取路径下所有的文件信息
+		List<FileCustom> listFile = fileService.listFile(realPath);
+		//将文件信息封装成Json对象
+		Result<List<FileCustom>> result = new Result<>(325, true, "获取成功");
+		result.setData(listFile);
+		return result;
+	}
 }
