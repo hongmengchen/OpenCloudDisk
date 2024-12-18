1ba376cf57c6f4edb6e65cf1446d4d79d5dd16fa
hongmengchen <2560174991@qq.com>
2024-12-18 19:45:22 +0800

    refactor(pojos): 继承 FileCustom 类并添加 EqualsAndHashCode 注解
    - 让 RecycleFile 类继承 FileCustom 类，扩展其功能
    - 添加 Lombok 的 EqualsAndHashCode 注解，自动生成 equals 和 hashCode 方法
    - 这些修改提高了代码的可维护性和扩展性

--- src/main/java/cn/ccs/pojo/RecycleFile.java
+++ src/main/java/cn/ccs/pojo/RecycleFile.java
@@ -1,9 +1,11 @@
 package cn.ccs.pojo;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class RecycleFile {
+public class RecycleFile extends FileCustom{
     // 文件id
     private Integer fileId;
 }
