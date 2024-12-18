e4c1ba4f9076862fb8f44460909dd06ce5fd0941
Coki_Lee <14337294+coki-lee@user.noreply.gitee.com>
2024-12-18 12:56:56 +0800

    feat(dao): 新增 officeDao 接口
    - 添加 officeDao 接口，包含处理办公信息的方法
    - 新增 addOffice 方法，用于添加办公记录
    - 新增 getOfficeId 方法，根据 MD5 获取办公 ID

---
+++ src/main/java/cn/ccs/dao/officeDao.java
@@ -0,0 +1,16 @@
+package cn.ccs.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface officeDao {
+
+    static void addOffice(@Param("officeId") String officeId, @Param("officeMd5") String officeMd5) throws Exception {
+
+    }
+
+    String getOfficeId(String officeMd5) throws Exception;
+}
