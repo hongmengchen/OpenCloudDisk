043ecc00804f560c266b960fd60f7dfc9d10534e
hongmengchen <2560174991@qq.com>
2024-12-18 10:48:58 +0800

    feat(file): 添加文件下载功能- 新增文件下载方法，支持单个或多个文件下载 - 使用 ResponseEntity 返回文件流，实现文件传输 - 优化文件名编码，解决中文文件名乱码问题 - 添加文件下载后的临时文件删除逻辑，节省服务器空间

--- src/main/java/cn/ccs/controller/FileController.java
+++ src/main/java/cn/ccs/controller/FileController.java
@@ -4,41 +4,78 @@ import cn.ccs.pojo.FileCustom;
 import cn.ccs.pojo.Result;
 import cn.ccs.service.impl.FileServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.List;
 
 @Controller
 @RequestMapping("/file")
 public class FileController {
-	private final HttpServletRequest request;
-	private final FileServiceImpl fileService;
+    private final HttpServletRequest request;
+    private final FileServiceImpl fileService;
 
-	@Autowired
+    @Autowired
     public FileController(FileServiceImpl fileService, HttpServletRequest request) {
         this.fileService = fileService;
-		this.request = request;
-	}
+        this.request = request;
+    }
 
-	/**
-	 * 获取文件列表
-	 *
-	 * @param path 路径
-	 * @return Json对象
-	 */
+    /**
+     * 获取文件列表
+     *
+     * @param path 路径
+     * @return Json对象
+     */
     @RequestMapping("/getFiles")
-	public @ResponseBody Result<List<FileCustom>> getFiles(String path) {
-		//根据项目路径及用户名、文件名获取上传文件的真实路径
-		// WEB-INF/file/ username/ fileName
-		String realPath = fileService.getFileName(request, path);
-		//获取路径下所有的文件信息
-		List<FileCustom> listFile = fileService.listFile(realPath);
-		//将文件信息封装成Json对象
-		Result<List<FileCustom>> result = new Result<>(325, true, "获取成功");
-		result.setData(listFile);
-		return result;
-	}
+    public @ResponseBody Result<List<FileCustom>> getFiles(String path) {
+        //根据项目路径及用户名、文件名获取上传文件的真实路径
+        // WEB-INF/file/ username/ fileName
+        String realPath = fileService.getFileName(request, path);
+        //获取路径下所有的文件信息
+        List<FileCustom> listFile = fileService.listFile(realPath);
+        //将文件信息封装成Json对象
+        Result<List<FileCustom>> result = new Result<>(325, true, "获取成功");
+        result.setData(listFile);
+        return result;
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param currentPath 当前路径
+     * @param downPath    文件名
+     * @param username    用户名
+     * @return 文件下载流
+     */
+    @RequestMapping("/download")
+    public ResponseEntity<byte[]> download(String currentPath, String[] downPath, String username) {
+        try {
+			// 获取文件名
+            String down = request.getParameter("downPath");
+			// 获取文件
+            File downloadFile = fileService.downPackage(request, currentPath, downPath, username);
+			// 设置文件头
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+			// 设置文件名
+            String fileName = new String(downloadFile.getName().getBytes("utf-8"), "iso-8859-1");
+            headers.setContentDispositionFormData("attachment", fileName);
+			// 读取文件
+            byte[] fileToByteArray = org.apache.commons.io.FileUtils.readFileToByteArray(downloadFile);
+            fileService.deleteDownPackage(downloadFile);
+			// 返回文件流
+            return new ResponseEntity<byte[]>(fileToByteArray, headers, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
