aecf23643dc0f6cd0b06578ed2e1ed8fa195d650
Coki_Lee <14337294+coki-lee@user.noreply.gitee.com>
2024-12-18 19:06:31 +0800

    feat(controller): 添加分享功能控制器- 新增 ShareController 类，实现文件分享功能 - 添加两个处理分享相关请求的处理方法： - shareFile：处理文件分享请求，生成分享链接   - share：处理分享链接访问请求，展示分享的文件 - 使用 Result 类封装返回结果，提高接口的可用性和可维护性

--- src/main/java/cn/ccs/dao/ShareDao.java
+++ src/main/java/cn/ccs/dao/ShareDao.java
@@ -1,7 +1,15 @@
 package cn.ccs.dao;
 
+import cn.ccs.pojo.Share;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface ShareDao {
+    void shareFile(Share share) throws Exception;
+    List<Share> findShareByName(@Param("username") String username, @Param("status")  int status) throws Exception;
+    void cancelShare(@Param("url") String url, @Param("filePath")  String filePath, @Param("status") int status) throws Exception;
+    List<Share> findShare(Share share) throws Exception;
 }
